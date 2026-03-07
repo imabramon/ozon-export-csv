5a7ec6cdb800968b74d02173010f8b50bd902db2
Vladislav Shelopugin <vshelopugin@pyrus.com>
2026-03-07 18:35:20 +0300

    add: useInjectFunction

--- src/hooks/useChromeApi.ts
+++ src/hooks/useChromeApi.ts
@@ -2,9 +2,39 @@ import { useEffectEvent } from "react";
 
 export const useChromeApi = () => {
   const openInNewTab = useEffectEvent((pageName: string) => {
-    const url = chrome.runtime.getURL(`src/pages/${pageName}/index.html`)
-    chrome.tabs.create({ url })
+    const url = chrome.runtime.getURL(`src/pages/${pageName}/index.html`);
+    chrome.tabs.create({ url });
   });
 
-  return {openInNewTab}
+  return { openInNewTab };
+};
+
+export const useInjectFunction = <F extends (...args: any[]) => any>(
+  func: F,
+) => {
+  return (...args: Parameters<F>): Promise<ReturnType<F> | undefined> => {
+    return new Promise<ReturnType<F> | undefined>((res, rej) => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0];
+        if (!tab?.id) {
+          rej(new Error("No active tab"));
+          return;
+        }
+
+        chrome.scripting
+          .executeScript({
+            target: { tabId: tab.id },
+            func,
+            args,
+          })
+          .then(
+            (results) => {
+              const result = results[0]?.result as ReturnType<F> | undefined;
+              res(result);
+            },
+            rej,
+          );
+      });
+    });
+  };
 };
--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -1,11 +1,15 @@
-import { useChromeApi } from "@/hooks/useChromeApi";
+import { useChromeApi, useInjectFunction } from "@/hooks/useChromeApi";
 
 export default function App() {
   const { openInNewTab } = useChromeApi();
+  const log = useInjectFunction(() => {
+    console.log('calll');
+  });
   return (
     <div>
       <h1>HelloWorld1</h1>
       <button onClick={() => openInNewTab("page")}>Открыть страницу</button>
+      <button onClick={() => log()}>Calll</button>
     </div>
   );
 }
