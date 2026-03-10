d99af99bc9039d0b2a4889494077ac3d930f1f6e
Vladislav Shelopugin <vshelopugin@pyrus.com>
2026-03-10 23:33:17 +0300

    add: icons

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,24 +1,27 @@
-import { defineManifest } from '@crxjs/vite-plugin'
-import pkg from './package.json'
+import { defineManifest } from "@crxjs/vite-plugin";
+import pkg from "./package.json";
 
 export default defineManifest({
   manifest_version: 3,
-  name: pkg.name,
+  name: "Экспорт выписки Ozon Банка",
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    "16": "public/ozon_logo@0.12x.png",
+    "32": "public/ozon_logo@0.25x.png",
+    "64": "public/ozon_logo@0.5x.png",
+    "128": "public/ozon_logo.png",
   },
   action: {
     default_icon: {
-      48: 'public/logo.png',
+      "64": "public/ozon_logo@0.5x.png",
     },
-    default_popup: 'src/pages/popup/index.html',
+    default_popup: "src/pages/popup/index.html",
   },
   permissions: [
-    'sidePanel',
-    'contentSettings',
-    'tabs',
-    'scripting',
-    'activeTab',
+    "sidePanel",
+    "contentSettings",
+    "tabs",
+    "scripting",
+    "activeTab",
   ],
-})
+});
